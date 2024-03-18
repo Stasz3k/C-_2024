4e5fa2cc9a0036253cca98c5b2d877700abd5407
Stasz3k <s24412@pjwstk.edu.pl>
2024-03-18 20:11:08 +0100

    konflikt

--- CSharp_1/Program.cs
+++ CSharp_1/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Modyfikacja 3!");
+﻿
+class Program
+{
+
+    // Statyczna metoda zwracająca wyliczony max z tablicy int
+    static double GetMax(int[] tab)
+    {
+        if (tab.Length > 0)
+        {
+            double maxValue = tab[0];
+            for (int i = 1; i < tab.Length; i++)
+            {
+                if (maxValue < tab[i])
+                    maxValue = tab[i];
+            }
+            return maxValue;
+        }
+        else throw new ArgumentException("Tablica musi zawierac elementy.", nameof(tab));
+    }
+    // Statyczna metoda zwracająca wyliczoną średnią z tablicy int
+    static double GetAverage(int[] tab)
+    {
+        if (tab.Length > 0)
+        {
+            double srednia = 0;
+            for (int i = 0; i < tab.Length; i++)
+            {
+                srednia += tab[i];
+            }
+            return srednia / tab.Length;
+        }
+        else throw new ArgumentException("Tablica musi zawierać elementy.", nameof(tab));
+    }
+
+    static void Main(string[] args)
+    {
+        int[] tablica = { 10, 20, 30, 40, 50 };
+
+        double maxValue = GetMax(tablica);
+        Console.WriteLine("Max z tablicy: " + maxValue);
+
+        double srednia = GetAverage(tablica);
+        Console.WriteLine("Średnia z tablicy: " + srednia);
+    }
+}
