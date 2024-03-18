1ac1a2f911dfdd3fa199f2e3e7da15e25e0fe034
Stasz3k <s24412@pjwstk.edu.pl>
2024-03-18 19:40:27 +0100

    Statyczna metoda max

--- CSharp_1/Program.cs
+++ CSharp_1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Modyfikacja 3!");
+﻿
+class Program
+{
+    // Statyczna metoda zwracająca wyliczony max z tablicy int
+    static double GetMax(int[] tab)
+    {
+        if (tab.Length > 0)
+        {
+            double maxValue = tab[0];
+            for (int i = 1; i < tab.Length; i++)
+            {
+                if (maxValue < tab[i])
+                    maxValue = tab[i];
+            }
+            return maxValue;
+        }
+        else throw new ArgumentException("Tablica musi zawierac elementy.", nameof(tab));
+    }
+
+    static void Main(string[] args)
+    {
+        int[] tablica = { 10, 20, 30, 40, 50 };
+
+        double maxValue = GetMax(tablica);
+        Console.WriteLine("Max z tablicy: " + maxValue);
+    }
+}
