07ff48c246b61a0470a091958341297596b79bb7
Stasz3k <s24412@pjwstk.edu.pl>
2024-03-18 19:25:30 +0100

    Statyczna metoda GetAverage

--- CSharp_1/Program.cs
+++ CSharp_1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Modyfikacja 3!");
+﻿
+class Program
+{
+    // Statyczna metoda zwracająca wyliczoną średnią z tablicy int
+    static double GetAverage(int[] tab)
+    {
+        double average = 0;
+        for (int i = 0; i < tab.Length; i++)
+        {
+            average += tab[i];
+        }
+        return average / tab.Length;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] tablica = { 10, 20, 30, 40, 50 };
+
+        double srednia = GetAverage(tablica);
+        Console.WriteLine("Średnia z tablicy: " + srednia);
+    }
+}
